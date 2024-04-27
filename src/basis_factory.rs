046b8c379b81571e056ef0ff1c4a9c84a9342b18
Josh Colclough <colcloughj2@cardiff.ac.uk>
2024-04-27 10:57:12 +0100

    Add the penrose tiling constraint.

--- src/basis_factory.rs
+++ src/basis_factory.rs
@@ -1,14 +1,46 @@
 use crate::core::RealSpace;
 use crate::linear_basis::LinearBasis;
 
+const fn fix_even(n: usize) -> usize {
+    if n % 2 == 0 { n / 2 } else { n }
+}
+
 pub struct BasisFactory {
 }
 
 impl BasisFactory {
+    fn calc_surf_vectors_rotsym<const S: usize>() -> [RealSpace<2>; fix_even(S)] {
+        let mut basis_vecs = [RealSpace::<2>::zeros(); fix_even(S)];
+        let mut angle = 0.0f32;
+        let incr = std::f32::consts::PI * 2.0/5.0;
+
+        for j in 0..fix_even(S) {
+            basis_vecs[j] = [angle.cos(), angle.sin()].into();
+            angle += incr;
+        }
+        basis_vecs
+    }
+
+    fn penrose_constraint<const I: usize>(mut offsets: [f32; I]) -> [f32; I] {
+        // Penrose constraint is that the sum of the offsets is 0.
+        // In order to sum to 0, make last offset equal to negative sum
+        // of the other numbers.
+        let mut sum_bar_last = 0.0;
+        for idx in 0..offsets.len() - 1 {
+            sum_bar_last += offsets[idx];
+        }
+        offsets[offsets.len() - 1] = -sum_bar_last;
+        offsets
+    }
+
+    pub fn surf_with_rotsym<const S: usize>(offsets: Option<[f32; fix_even(S)]>) -> LinearBasis<2, { fix_even(S) }> {
+        LinearBasis::<2, {fix_even(S)}>::from_vectors(Self::calc_surf_vectors_rotsym::<S>(), offsets.unwrap_or([0.1; fix_even(S)]))
+    }
+
     pub fn cubic2d(offsets: Option<[f32; 2]>) -> LinearBasis<2, 2> {
         Self::cubic::<2>(offsets)
     }
-
+    
     pub fn cubic<const D: usize>(offsets: Option<[f32; D]>) -> LinearBasis<D, D> {
         let mut basis_vecs = [RealSpace::<D>::zeros(); D];
         for (j, v) in basis_vecs.iter_mut().enumerate() {
@@ -19,15 +51,14 @@ impl BasisFactory {
     }
 
     pub fn penrose(offsets: Option<[f32; 5]>) -> LinearBasis<2, 5> {
-        let mut basis_vecs = [RealSpace::<2>::zeros(); 5];
-        let mut angle = 0.0f32;
-        let incr = std::f32::consts::PI * 2.0/5.0;
-
-        for j in 0..5 {
-            basis_vecs[j] = [angle.cos(), angle.sin()].into();
-            angle += incr;
-        }
-        LinearBasis::<2, 5>::from_vectors(basis_vecs, offsets.unwrap_or([0.1; 5]))
+        // Penrose is a special case where offsets sum to 0.
+        let offsets_clamped = if let Some(ofst) = offsets {
+            Self::penrose_constraint(ofst)
+        } else {
+            [0.2, 0.2, 0.2, 0.2, -0.8]
+        };
+        LinearBasis::<2, 5>::from_vectors(Self::calc_surf_vectors_rotsym::<5>(),
+                                          offsets_clamped)
     }
 }
 
